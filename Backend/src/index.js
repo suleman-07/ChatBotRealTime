86e078e75a7b77dc9c496cc4288f1a557d35da3d
Umair <umair.naseem55552@gmail.com>
2026-06-20 13:51:01 +0500

    Replcae the cors local to production url

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 app.use(cors(
-    {origin: "http://localhost:5173",
+    {origin: "https://chat-bot-real-time-8cxgs1xa9-suleman-s-projects1.vercel.app",
     credentials: true,}
 
 ));
--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://chat-bot-real-time-8cxgs1xa9-suleman-s-projects1.vercel.app"],
   },
 });
 

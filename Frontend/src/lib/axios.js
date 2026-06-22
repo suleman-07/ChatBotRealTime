bbacab2cff49d72e80eb5bccf759f7a60e994ed6
Umair <umair.naseem55552@gmail.com>
2026-06-22 13:38:12 +0500

    Ready for deployment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5002/api",
+    baseURL: "https://chatbotrealtime-production.up.railway.app/api",
     withCredentials: true,
 
 });

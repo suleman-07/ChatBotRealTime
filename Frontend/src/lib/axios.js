b994485f7e289b03771486510b84eb2bd2be46aa
Umair <umair.naseem55552@gmail.com>
2026-06-22 13:00:00 +0500

    Port fixed

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "http://localhost:5002/api",
     withCredentials: true,
 
 });
